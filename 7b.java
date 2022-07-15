ba7107eec8fda944d3301e3d30301f1b8443d394
vikhyat-3 <82944741+vikhyat-3@users.noreply.github.com>
2022-07-15 22:05:58 +0530

    Update 7b.java

--- 7b.java
+++ 7b.java
@@ -1,59 +1,44 @@
-import java.util.*;
-public class round_robin {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int q,rp,n;
-		System.out.println("Enter no. of processes");
-		n=sc.nextInt();
-		int bt[]=new int[n];
-		int rt[]=new int[n];
-		int wt[]=new int[n];
-		System.out.println("Enter burst time for each process");
-		for(int i=0;i<n;i++) {
-			bt[i]=sc.nextInt();
-			rt[i]=bt[i];
-		}
-		System.out.println("Enter quantum time");
-		q=sc.nextInt();
-		rp=n;
-		int time=0,i=0;
-		while(rp>0) {
-			if(rt[i]>q) {
-				rt[i]-=q;
-				time+=q;
-				System.out.println("Process["+i+"]");
-				for(int k: rt)
-					System.out.print(k);
-			}
-			else if(rt[i]<=q&&rt[i]>0) {
-				time+=rt[i];
-				rt[i]=0;
-				System.out.println("Process["+i+"] terminated");
-				rp--;
-			}
-			for(int p=0;p<n;p++) {
-				if(rt[p]==0)
-				{
-					continue;
-				}
-				wt[p]+=q;
-			}
-			i=(i+1)%n;
-		}
-		/*int total=0;
-		for(int p:wt)
-			total+=p;
-		float avg_wt=((float)total)/n;
-
-		total=0;
-		for(int p=0;i<n;i++) {
-			tat[p]=wt[p]+bt[p];
-		}
-		float avg_wt=((float)total)/n;*/
-
-		System.out.println("Total time: "+time);
-	}
-
+import java.util.*;import java.util.Scanner;
+public class TestClass {
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    int wtime[], btime[], rtime[], num, quantum, total;
+    wtime = new int[10];
+    btime = new int[10];
+    rtime = new int[10];
+    System.out.print("Enter number of processes(MAX 10): ");
+    num = s.nextInt();
+    System.out.print("Enter burst time");
+    for (int i = 0; i < num; i++) {
+      System.out.print("\nP[" + (i + 1) + "]:");
+      btime[i] = s.nextInt();
+      rtime[i] = btime[i];
+      wtime[i] = 0;
+    }
+    System.out.print("\n\nEnter quantum: ");
+    quantum = s.nextInt();
+    int rp = num;
+    int i = 0;
+    int time = 0;
+    System.out.print("0");
+    wtime[0] = 0;
+    while (rp != 0) {
+      if (rtime[i] > quantum) {
+        rtime[i] = rtime[i] - quantum;
+        System.out.print(" | P[" + (i + 1) + "] | ");
+        time += quantum;
+        System.out.print(time);
+      } else if (rtime[i] <= quantum && rtime[i] > 0) {
+        time += rtime[i];
+        rtime[i] = rtime[i] - rtime[i];
+        System.out.print(" | P[" + (i + 1) + "] | ");
+        rp--;
+        System.out.print(time);
+      }
+      i++;
+      if (i == num) {
+        i = 0;
+      }
+    }
+  }
 }
